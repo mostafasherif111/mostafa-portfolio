e17935353578776087f9ac18df2bd14a35482a74
mostafasherif111 <ms0400123@gmail.com>
2026-06-29 08:29:04 +0300

    update

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="ERoLF49TbWJ5bVDD_EtnLmUWNoVlXidAu60H46KLADg"
+      />
+    </>
+  );
+}
